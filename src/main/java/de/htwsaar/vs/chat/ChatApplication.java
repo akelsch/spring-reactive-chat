173afb528ffba5c17186adbdb65e2b19520f0238
Arthur Kelsch <akelsch@users.noreply.github.com>
2019-02-18 23:46:48 +0100

    Add EnableMongoAuditing annotation so CreatedDate works properly

--- src/main/java/de/htwsaar/vs/chat/ChatApplication.java
+++ src/main/java/de/htwsaar/vs/chat/ChatApplication.java
@@ -2,8 +2,10 @@ package de.htwsaar.vs.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class ChatApplication {
 
     public static void main(String[] args) {
