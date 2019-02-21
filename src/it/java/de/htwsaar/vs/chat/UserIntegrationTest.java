553ff1f3fea7a5ae7d4c7bf083ff9b4878a2e398
LeslieM98 <leslie.marxen98@gmail.com>
2019-02-21 01:35:55 +0100

    #21: Rewrote tests and removed regEx

--- src/it/java/de/htwsaar/vs/chat/UserIntegrationTest.java
+++ src/it/java/de/htwsaar/vs/chat/UserIntegrationTest.java
@@ -4,10 +4,8 @@ import de.htwsaar.vs.chat.auth.Role;
 import de.htwsaar.vs.chat.model.User;
 import de.htwsaar.vs.chat.repository.UserRepository;
 import de.htwsaar.vs.chat.router.AuthRouter;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -17,12 +15,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpHeaders.LOCATION;
@@ -40,12 +36,16 @@ public class UserIntegrationTest  {
     @Autowired
     private UserRepository userRepository;
 
-    private final User users[] = new User[20];
+    private final List<User> users = new ArrayList<>();
 
     @BeforeEach
     void setUp(){
-        final StringBuilder jsonBuilder = new StringBuilder();
-        for(int i = 0; i < users.length; i++){
+        userRepository.deleteAll();
+        users.clear();
+
+        users.addAll(userRepository.findAll().collectList().block());
+        users.stream().forEach(x -> x.setPassword(null));
+        for(int i = 0; i < 20; i++){
             User u = new User();
 
             u.setUsername(String.format("t%d", i));
@@ -60,16 +60,13 @@ public class UserIntegrationTest  {
                     break;
             }
 
+
             userRepository.save(u).subscribe();
-            users[i] = u;
+            u.setPassword(null);
+            users.add(u);
 
 
         }
-    }
-
-    @AfterEach
-    void clearUp(){
-        userRepository.deleteAll();
     }
 
 
@@ -77,51 +74,69 @@ public class UserIntegrationTest  {
     @WithMockUser(username = "t0", password = "p0")
     void getAll(){
 
-        webTestClient
+        final List<User> tmp = Arrays.asList(webTestClient
                 .get().uri("/api/v1/users")
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(String.class).value(Pattern.compile("(.*\"id\".*){" + users.length + "}")::matcher);
-        // contains users.length users
-    }
+                .expectBody(User[].class).returnResult().getResponseBody());
 
+
+        Assertions.assertTrue(tmp.containsAll(users));
+    }
     @Test
     @WithMockUser(username = "t0", password = "p0")
     void getAdmins(){
-        webTestClient
-                .get().uri("/api/v1/users?group=ADMIN")
+
+
+        final List<User> tmp = Arrays.asList(webTestClient
+                .get().uri("/api/v1/users?roles=ADMIN")
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(String.class).value(Pattern.compile("(.*\"id\".*){" + (users.length / 2) + "}")::matcher);
+                .expectBody(User[].class).returnResult().getResponseBody());
+
+        Assertions.assertTrue(tmp.containsAll(users.stream()
+                .filter(x -> x.getRoles().contains(Role.ADMIN))
+                .collect(Collectors.toList())
+        ));
     }
 
     @Test
     @WithMockUser(username = "t0", password = "p0")
     void getUsers(){
-        webTestClient
-                .get().uri("/api/v1/users?group=USER")
+        final List<User> tmp = Arrays.asList(webTestClient
+                .get().uri("/api/v1/users")
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(String.class).value(Pattern.compile("(.*\"id\".*){" + users.length + "}")::matcher);
+                .expectBody(User[].class).returnResult().getResponseBody());
+
+        Assertions.assertTrue(tmp.containsAll(users.stream()
+                .filter(x -> x.getRoles().contains(Role.USER))
+                .collect(Collectors.toList())
+        ));
     }
 
     @Test
     @WithMockUser(username = "t0", password = "p0")
     void getSingleUser(){
-        final User u = userRepository.findByUsername(users[0].getUsername()).block();
+        final int rand = new Random().nextInt(users.size());
+        final User u = userRepository.findByUsername(users.get(rand).getUsername()).block();
 
-        webTestClient
+        final List<User> tmp = Arrays.asList(webTestClient
                 .get().uri("/api/v1/users/" + u.getId())
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(String.class).value(Pattern.compile(String.format("(.*%s.*)", u.getId()))::matcher);
+                .expectBody(User[].class).returnResult().getResponseBody());
+
+        Assertions.assertEquals(1, tmp.size());
+        Assertions.assertTrue(tmp.containsAll(users));
 
     }
 
     @Test
     @WithMockUser(username = "t0", password = "p0")
     void deleteUser(){
-        final User u = userRepository.findByUsername(users[0].getUsername()).block();
+
+        final User u = userRepository.findByUsername(users.get(0).getUsername()).block();
         final String id = u.getId();
 
         webTestClient
@@ -138,7 +153,7 @@ public class UserIntegrationTest  {
     @Test
     @WithMockUser(username = "t0", password = "p0")
     void changePassword(){
-        final User u = userRepository.findByUsername(users[0].getUsername()).block();
+        final User u = userRepository.findByUsername(users.get(0).getUsername()).block();
 
         webTestClient
                 .post().uri(String.format("/api/v1/users/%s/changePassword", u.getId()))
@@ -158,10 +173,6 @@ public class UserIntegrationTest  {
                 .exchange()
                 .expectStatus().isNotFound();
     }
-
-
-
-
 
 
 }
