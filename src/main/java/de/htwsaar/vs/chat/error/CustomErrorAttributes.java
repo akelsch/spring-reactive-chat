60488422cd6f2fd9e39e2e4d66bddeb57d4be1f2
Arthur Kelsch <akelsch@users.noreply.github.com>
2019-03-05 17:38:23 +0100

    Polish formatting, naming, docs, etc before merging

--- src/main/java/de/htwsaar/vs/chat/error/CustomErrorAttributes.java
+++ src/main/java/de/htwsaar/vs/chat/error/CustomErrorAttributes.java
@@ -1,10 +1,6 @@
-package de.htwsaar.vs.chat.exception;
-
-import java.util.Map;
-import java.util.Optional;
+package de.htwsaar.vs.chat.error;
 
 import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
-
 import org.springframework.core.codec.DecodingException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
@@ -12,38 +8,43 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 
 import javax.validation.ConstraintViolationException;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
 
 /**
+ * Extends {@link DefaultErrorAttributes} to change the response HTTP status code
+ * in case of well known exceptions.
  *
  * @author Julian Quint
  */
 @Component
-public class GlobalErrorAttributes extends DefaultErrorAttributes {
+public class CustomErrorAttributes extends DefaultErrorAttributes {
 
     @Override
     public Map<String, Object> getErrorAttributes(ServerRequest request, boolean includeStackTrace) {
-        Map<String, Object> map = super.getErrorAttributes(request, includeStackTrace);
+        Map<String, Object> errorAttributes = super.getErrorAttributes(request, includeStackTrace);
         Optional<HttpStatus> errorStatus = determineHttpStatus(getError(request));
+
         errorStatus.ifPresent(httpStatus -> {
-            map.replace("status", httpStatus.value());
-            map.replace("error", httpStatus.getReasonPhrase());
+            errorAttributes.replace("status", httpStatus.value());
+            errorAttributes.replace("error", httpStatus.getReasonPhrase());
         });
-        return map;
+
+        return errorAttributes;
     }
 
     private Optional<HttpStatus> determineHttpStatus(Throwable error) {
         if (error instanceof DecodingException || error instanceof ConstraintViolationException) {
             return Optional.of(BAD_REQUEST);
         }
+
         if (error instanceof DuplicateKeyException) {
             return Optional.of(CONFLICT);
         }
+
         return Optional.empty();
     }
-
 }
-
-
