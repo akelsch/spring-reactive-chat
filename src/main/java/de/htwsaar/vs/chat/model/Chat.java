157c19dc853db913056042039103b0b7614c4f45
Arthur Kelsch <akelsch@users.noreply.github.com>
2019-03-07 14:22:19 +0100

    Serialize members in Chat and make name optional for 1:1 chats

--- src/main/java/de/htwsaar/vs/chat/model/Chat.java
+++ src/main/java/de/htwsaar/vs/chat/model/Chat.java
@@ -1,14 +1,11 @@
 package de.htwsaar.vs.chat.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import de.htwsaar.vs.chat.model.serializer.CollectionSizeSerializer;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
@@ -24,11 +21,9 @@ import java.util.Set;
 @Document
 public class Chat extends BaseDocument {
 
-    @NotBlank
     private String name;
 
     @NotNull
-    @JsonSerialize(using = CollectionSizeSerializer.class)
     @DBRef
     private Set<User> members;
 }
