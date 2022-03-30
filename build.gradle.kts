d16dfe07cfe5073acdb60ef37c5d3335b1d85bbd
Arthur Kelsch <akelsch@users.noreply.github.com>
2022-03-30 23:18:40 +0200

    Update to Spring Boot 2.3.12.RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.2.13.RELEASE"
+    id("org.springframework.boot") version "2.3.12.RELEASE"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.adarshr.test-logger") version "3.2.0"
     id("org.unbroken-dome.test-sets") version "4.0.0"
@@ -20,7 +20,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("com.auth0:java-jwt:3.12.0")
+    implementation("com.auth0:java-jwt:3.18.2")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
