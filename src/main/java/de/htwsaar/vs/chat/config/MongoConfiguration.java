642f793bd29a814f562ba4129cbba0687ce4cd0b
Arthur Kelsch <akelsch@users.noreply.github.com>
2022-04-03 03:58:57 +0200

    Remove ValidationConfiguration

--- src/main/java/de/htwsaar/vs/chat/config/MongoConfiguration.java
+++ src/main/java/de/htwsaar/vs/chat/config/MongoConfiguration.java
@@ -1,7 +1,11 @@
 package de.htwsaar.vs.chat.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+
+import javax.validation.Validator;
 
 /**
  * Configuration class for Spring Data MongoDB.
@@ -11,4 +15,9 @@ import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 @Configuration
 @EnableReactiveMongoAuditing
 public class MongoConfiguration {
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener(Validator validator) {
+        return new ValidatingMongoEventListener(validator);
+    }
 }
