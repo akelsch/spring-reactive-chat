e84fef75a77eb6c982f44c4dedb3d44869f2beb9
Arthur Kelsch <akelsch@users.noreply.github.com>
2022-04-05 01:52:51 +0200

    Refactor JwtAuthenticationConverter

--- src/main/java/de/htwsaar/vs/chat/auth/jwt/JwtAuthenticationConverter.java
+++ src/main/java/de/htwsaar/vs/chat/auth/jwt/JwtAuthenticationConverter.java
@@ -4,15 +4,16 @@ import de.htwsaar.vs.chat.model.User;
 import org.springframework.core.ResolvableType;
 import org.springframework.core.codec.Decoder;
 import org.springframework.core.codec.Hints;
-import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 /**
  * Converts a JSON payload from a request into an {@link Authentication} object.
@@ -25,12 +26,18 @@ public class JwtAuthenticationConverter implements ServerAuthenticationConverter
 
     @Override
     public Mono<Authentication> convert(ServerWebExchange exchange) {
-        Flux<DataBuffer> body = exchange.getRequest().getBody();
+        ServerHttpRequest request = exchange.getRequest();
+        MediaType contentType = getContentType(request);
 
         return decoder
-                .decodeToMono(body, ResolvableType.forType(User.class), MediaType.APPLICATION_JSON, Hints.none())
+                .decodeToMono(request.getBody(), ResolvableType.forClass(User.class), contentType, Hints.none())
                 .cast(User.class)
                 .filter(user -> user.getPassword() != null)
                 .map(u -> new UsernamePasswordAuthenticationToken(u.getUsername(), u.getPassword()));
+    }
+
+    private MediaType getContentType(ServerHttpRequest request) {
+        MediaType contentType = request.getHeaders().getContentType();
+        return Objects.requireNonNullElse(contentType, MediaType.APPLICATION_JSON);
     }
 }
